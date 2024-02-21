e0a2f872bda06e8597db11225b1f2d92f350edba
Michael Carroll <mjcarroll@intrinsic.ai>
2024-02-21 13:47:54 +0000

    Example of using gtest with and without ament
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>

---
+++ using_gtest.cc
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+// Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
+int Factorial(int n) {
+  int result = 1;
+  for (int i = 1; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+
+TEST(using_gtest, hello_world) {
+  EXPECT_EQ(1, Factorial(1));
+  EXPECT_EQ(2, Factorial(2));
+  EXPECT_EQ(6, Factorial(3));
+  EXPECT_EQ(40320, Factorial(8));
+}
